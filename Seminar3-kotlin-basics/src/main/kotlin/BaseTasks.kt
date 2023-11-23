59b636071d6e5aeb6275d36467752ef1e7937bf5
Aleksa Khruleva <66669044+AleksaKhruleva@users.noreply.github.com>
2023-11-23 20:46:44 +0300

    BaseTasks.kt minor fixes

--- Seminar3-kotlin-basics/src/main/kotlin/BaseTasks.kt
+++ Seminar3-kotlin-basics/src/main/kotlin/BaseTasks.kt
@@ -1,24 +1,32 @@
 import kotlin.math.sqrt
 
 fun askForInput() {
+    print("Ваша фамилия : ")
     val surname = readln()
+    print("Ваше имя     : ")
     val name = readln()
+    print("Ваше отчество: ")
     val lastName = readln()
 
     println(
         """
-        Фамилия:  ${surname}
-        Имя:      ${name}
+        Фамилия : ${surname}
+        Имя     : ${name}
         Отчество: ${lastName}
         """.trimIndent())
 }
 
 fun quadraticRoots(a: Double, b: Double, c: Double): Pair<Double, Double> {
     val d = b * b - 4 * a * c
-    val x1 = (-b + sqrt(d)) / (2 * a)
-    val x2 = (-b - sqrt(d)) / (2 * a)
 
-    return x1 to x2
+    return if (d < 0) {
+        println("Дискриминант отрицательный :(")
+        0.0 to 0.0
+    } else {
+        val x1 = (-b + sqrt(d)) / (2 * a)
+        val x2 = (-b - sqrt(d)) / (2 * a)
+        x1 to x2
+    }
 }
 
 fun printSameDigitNumbers() {

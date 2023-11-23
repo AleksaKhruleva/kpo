a7146ca519e5f7e3e1cedbf2489b0a78c9d6c817
Aleksa Khruleva <66669044+AleksaKhruleva@users.noreply.github.com>
2023-11-23 21:00:43 +0300

    ArrayTasks.kt solution

--- Seminar3-kotlin-basics/src/main/kotlin/ArraysTasks.kt
+++ Seminar3-kotlin-basics/src/main/kotlin/ArraysTasks.kt
@@ -1,29 +1,28 @@
-
-/**
-    Задание 1: Сформировать целочисленный массив A из N элементов
-    при помощи ввода значений с клавиатуры (N вводит пользователь).
-    Вывести массив на экран. (1 балл)
- */
-
 fun makeArray(): Array<Int> {
+    print("Введите n - количество элементов массива: ")
     val n = readln().toInt()
-    return arrayOf(n)
+
+    val arr = Array<Int>(n) { 0 }
+
+    for (i in 0..<n) {
+        print("Введите ${i}-й элемент: ")
+        val num = readln().toInt()
+        arr[i] = num
+    }
+
+    print("Ваш массив: ")
+    println(arr.joinToString(" "))
+
+    return arr
 }
 
-/**
-    Задание 2: Написать функцию для сортировки массива по убыванию и его вывода на экран.
-    Использовать встроенные функции. (0.5 балла)
- */
 fun sortArrayDesc(arr: Array<Int>) {
-
+    arr.sortDescending()
+    print("Ваш массив отсортированный по убыванию: ")
+    println(arr.joinToString(" "))
 }
 
-// Используйте эту функцию для запуска кода
-// Раскомментируйте нужные участки в процессе реализации
 fun main() {
     val arr = makeArray()
-    println(arr.size)
-
-//    sortArrayDesc(arr)
+    sortArrayDesc(arr)
 }
-
